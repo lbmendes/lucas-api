54681c8bb99de398979be2664b7d383e1974d9f2
lbmendes <lbmendes@users.noreply.github.com>
2021-09-25 16:50:31 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="lbmendes"
+original_name="lucas_api"
+original_urlname="lucas-api"
+original_description="Awesome lucas_api created by lbmendes"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv lucas_api $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
